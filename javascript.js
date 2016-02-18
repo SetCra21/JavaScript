58f6858d4bc7727ea7f125cb8a6bf8453e3c3286
SetrigeCrawford <setrige.crawford@journalism.cuny.edu>
2016-02-17 19:46:17 -0500

    second attempt at updating java assignment

--- javascript.js
+++ javascript.js
@@ -1,6 +1,10 @@
-var x = 600;
-var y = 1000;
+var x = 900000;
+var y = 1500;
 var z = x + y;
 var a = x - y;
 var b = x * y;
 var c = x / y;
+document.getElementById("add").innerHTML = "If I have" + x + "and I add" + y + "I get" + z;
+document.getElementById("subtract").innerHTML = "If I have" + x + "and you take" + y + "Im left with" + a;
+document.getElementById("multiply").innerHTML = "If I keep" + x + "and multiply it by" + y + "I have" + b;
+document.getElementById("divide").innerHTML = "If I split" + x + "amongst" + y + "people everyone gets" + c;
